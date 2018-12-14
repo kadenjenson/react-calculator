c7e5a120b1f41772a1ad1f601ab48b440d58f443
kadenjenson <sobeit99@gmail.com>
2018-12-14 13:37:43 -0700

    refactored turnary operator

--- src/components/button.js
+++ src/components/button.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function isOperator(val) {
-  return !isNaN(val) || val === "." || val === "=";
+  return isNaN(val) || val === "." || val === "=";
 }
 
 export const Button = props => (
-  <div className={`button-wrapper ${isOperator(props.children) ? null : "operator"}`} onClick={() => props.handleClick(props.children)}>
+  <div className={`button-wrapper ${isOperator(props.children) ? "operator" : null }`} onClick={() => props.handleClick(props.children)}>
     {props.children}
   </div>
 )
